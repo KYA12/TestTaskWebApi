638f506afd5151d545adb722080235d9414c2646
KYA12 <kopyal12345@gmail.com>
2020-03-25 16:10:56 +0200

    delete files and add Database scrips file

--- TestAppWebApi/DBScripts/DatabaseScript.sql
+++ TestAppWebApi/DBScripts/DatabaseScript.sql
@@ -110,4 +110,87 @@ GO
 ALTER DATABASE [ShopDataBase] SET  READ_WRITE 
 GO
 
+USE [ShopDataBase]
+GO
 
+/****** Object:  Table [dbo].[Shop]    Script Date: 24.03.2020 17:18:42 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Shop](
+	[ShopID] [int] IDENTITY(1,1) NOT NULL,
+	[ShopName] [nvarchar](50) NOT NULL,
+	[Address] [nvarchar](50) NOT NULL,
+ CONSTRAINT [PK_Shop] PRIMARY KEY CLUSTERED 
+(
+	[ShopID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
+ CONSTRAINT [AK_ShopName] UNIQUE NONCLUSTERED 
+(
+	[ShopName] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+USE [ShopDataBase]
+GO
+
+/****** Object:  Table [dbo].[Consultant]    Script Date: 25.03.2020 15:44:56 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Consultant](
+	[ConsultantID] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](50) NOT NULL,
+	[Surname] [nvarchar](50) NOT NULL,
+	[DateHiring] [datetime] NULL,
+	[ShopID] [int] NULL,
+ CONSTRAINT [PK_Consultant] PRIMARY KEY CLUSTERED 
+(
+	[ConsultantID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Consultant] ADD  CONSTRAINT [DF_Consultant_DateHiring]  DEFAULT (getdate()) FOR [DateHiring]
+GO
+
+ALTER TABLE [dbo].[Consultant]  WITH CHECK ADD  CONSTRAINT [FK_Consultant_Shop] FOREIGN KEY([ShopID])
+REFERENCES [dbo].[Shop] ([ShopID])
+ON UPDATE CASCADE
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [dbo].[Consultant] CHECK CONSTRAINT [FK_Consultant_Shop]
+GO
+
+USE [ShopDataBase]
+GO
+
+INSERT INTO [dbo].[Shop]
+           ([ShopName]
+           ,[Address])
+     VALUES
+           ('Снежинка','Ярославская 47'),
+		   ('Орион','Садовая 23'),
+		   ('Рыбалка','Трамвайная 47')
+GO
+
+USE [ShopDataBase]
+GO
+
+INSERT INTO [dbo].[Consultant]
+           ([Name]
+           ,[Surname]
+		   ,[ShopID])
+     VALUES
+           ('Иван', 'Гринченко', 1),
+		   ('Андрей', 'Лукьянов', 2),
+		   ('Алексей', 'Романов', 3)
+GO
